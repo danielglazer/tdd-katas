c687897f31f9c27f10ac1f6cca874f5298e1620f
Daniel Glazer <danielglazer123@gmail.com>
2020-09-20 23:46:11 +0300

    feat(MineSweeper): finsh MineSweeper tdd kata

--- MineSweeper/src/MineSweeper.spec.ts
+++ MineSweeper/src/MineSweeper.spec.ts
@@ -1,14 +1,27 @@
 import { MineSweeper } from './MineSweeper';
-describe('FizzBuzz', () => {
+describe('MineSweeper', () => {
 	let mineSweeper: MineSweeper;
 
 	beforeEach(() => {
-		mineSweeper = new MineSweeper(0, 3);
+
 	});
 	test(`create empty map`, () => {
+		mineSweeper = new MineSweeper(0, 3);
 		const resultMap = [
 			[0, 0, 0],
 			[0, 0, 0],
+			[0, 0, 0]
+		]
+		expect(mineSweeper.createMap()).toStrictEqual(resultMap);
+	});
+	test(`create empty map`, () => {
+		const mockMath = Object.create(global.Math);
+		mockMath.random = () => 0;
+		global.Math = mockMath;
+		mineSweeper = new MineSweeper(1, 3);
+		const resultMap = [
+			[-1, 1, 0],
+			[1, 1, 0],
 			[0, 0, 0]
 		]
 		expect(mineSweeper.createMap()).toStrictEqual(resultMap);
--- MineSweeper/src/MineSweeper.ts
+++ MineSweeper/src/MineSweeper.ts
@@ -1,13 +1,57 @@
 export class MineSweeper {
+	private map: number[][];
 
 	constructor(private mines: number, private rows: number, private columns: number = rows) {
+		if (!this.isValidInput(mines, rows, columns)) {
+			console.log('Input not valid!');
+			this.map = [];
+			return;
+		}
+		const map: number[][] = []; // size rows X columns
+		const open: number[][] = [];
+		// initialize map
+		for (let i = 0; i < rows; i++) {
+			map.push([]);
+			for (let j = 0; j < columns; j++) {
+				open.push([i, j]);
+				map[i].push(0);
+			}
+		}
+		// put mines in map
+		for (let placedMines = 0; placedMines < mines; placedMines++) {
+			const leftSpaces = open.length;
+			const chosenIndex = Math.floor(Math.random() * leftSpaces);
+			let row, column;
+			[row, column] = open[chosenIndex];
+			// put mine and register neighbors
+			putMine(row, column, map);
+			open.splice(chosenIndex, 1);
+		}
+		this.map = map;
 	}
 
 	createMap(): number[][] {
-		return [
-			[0, 0, 0],
-			[0, 0, 0],
-			[0, 0, 0]
-		];
+		console.log(this.map);
+		return this.map;
 	}
+
+	isValidInput(private mines: number, private rows: number, private columns: number): boolean {
+		return mines >= 0 && rows > 0 && columns > 0 && mines <= rows * columns;
+	}
+}
+
+
+function putMine(row: number, column: number, map: number[][]) {
+	// put the mine
+	map[row][column] = -1;
+	// register neighbors
+	const validRowLow = Math.max(0, row - 1);
+	const validRowHigh = Math.min(map.length - 1, row + 1);
+	const validColLow = Math.max(0, column - 1);
+	const validColHigh = Math.min(map[0].length - 1, column + 1);
+	; console.log([validRowLow, validRowHigh, validColLow, validColHigh]);
+	for (let i = validRowLow; i <= validRowHigh; i++)
+		for (let j = validColLow; j <= validColHigh; j++)
+			if (map[i][j] !== -1)
+				map[i][j]++;
 }
